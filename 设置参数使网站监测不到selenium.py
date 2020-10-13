24c93040bb036d577c7151da33f39a7ee52ba30c
xuepengswh <l826898728@163.com>
2020-10-13 17:52:46 +0800

    12

--- 设置参数使网站监测不到selenium.py
+++ 设置参数使网站监测不到selenium.py
@@ -1,18 +1,25 @@
-options = webdriver.ChromeOptions()
-    # 设置为开发者模式，防止网站识别
-    options.add_experimental_option(
-        'excludeSwitches', ['enable-automation'])
-    options.add_experimental_option("useAutomationExtension", False)
-    options.add_argument(
-        "user-agent=Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)")
-    # 加载驱动程序
-    browser = webdriver.Chrome(executable_path='./chromedriver',
-                               options=options)
+from selenium import webdriver
 
-    browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+
+options = webdriver.ChromeOptions()
+
+# 设置为开发者模式，防止网站识别
+options.add_experimental_option('excludeSwitches', ['enable-automation'])
+options.add_experimental_option("useAutomationExtension", False)
+options.add_argument("user-agent=Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)")
+
+# 加载驱动程序
+browser = webdriver.Chrome(options=options)
+
+browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
         "source": """
         Object.defineProperty(navigator, 'webdriver', {
           get: () => undefined
         })
       """
     })
+
+
+url = "http://www.caac.gov.cn/XXGK/XXGK/TZTG/202009/t20200922_204613.html"
+browser.get(url)
+
