ac32816933f1cc438fff25007fd50c9d1a929ba4
xuepengswh <l826898728@163.com>
2020-11-16 18:31:24 +0800

    11169

--- get-news-url.py
+++ get-news-url.py
@@ -9,7 +9,10 @@ import sys
 
 #获取html文本
 def get_html(url):
-    ps = requests.get(url).content.decode("utf-8")
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
+    }
+    ps = requests.get(url, headers=headers, verify=False).content.decode("utf-8")
     return ps
 
 #获取所有a链接的路径
@@ -19,8 +22,8 @@ def get_all_path(ps):
 
     d = defaultdict(list)
     for href in hrefs:
-        if href.text == None or href.text.strip() == '':
-            continue
+        # if href.text == None or href.text.strip() == '':
+        #     continue
         if 'href' not in href.attrib:
             continue
         # if href.text in [u'下一页', 'next']:
@@ -37,11 +40,10 @@ def get_all_path(ps):
                 path.append("/"+tag_name)
             parent = parent[0].xpath('..')
         path = ' '.join(path)
-        text = href.text.strip()
+        text = None
         url = href.attrib['href']
         d[path].append((text, url))
-        return d
-
+    return d
 
 #从k值变成xpath字符串
 def k_to_xpath(k):
@@ -77,14 +79,17 @@ def k_to_txtnum(k):
         return 0
     url = url[0]
     url = urljoin(base_url,url)
-    ps = get_html(url)
+    try:
+        ps = get_html(url)
+    except:     #网络请求出现错误(gopage)等，返回0
+        return 0
     num = get_num_ratio(ps) #网页文字数量和链接数量的比值
     return num
 
 #根据文章字数数量确定链接路径
 def get_best_path(d):
-    for k, v in d.items():
-        print(k, v)
+    # for k, v in d.items():
+    #     print(k, v)
     best_k = 0
     best_v = 0
     for k, v in d.items():
@@ -95,6 +100,7 @@ def get_best_path(d):
 
     for e in d[best_k]:
         print(e[0], e[1])
+
     end_path = k_to_xpath(best_k)
     print("best_path")
     print(end_path)
@@ -102,6 +108,9 @@ def get_best_path(d):
 
 def go():
     d = get_all_path(base_ps)    #获取所有a链接路径
+    if not d:   #处理url 404等错误
+        print("没有获取到数据")
+        return
     best_path = get_best_path(d)    #获取最优路径
     mytree = etree.HTML(base_ps)
 
@@ -111,6 +120,6 @@ def go():
     print(len(linelist))
 
 if __name__=="__main__":
-    base_url = "http://www.customs.gov.cn/eportal/ui?pageId=302272&currentPage=1&moduleId=b600eb6d22ff4eca8712ae56dd689014&staticRequest=yes"
+    base_url = "http://service.shanghai.gov.cn/XingZhengWenDangKu/XZGFList.aspx?testpara=0&kw=&issueDate_userprop8=&status=0&departid=&wenhao=&issueDate_userprop8_end=&excuteDate=&excuteDate_end=&closeDate=&closeDate_end=&departtypename=&typename=&zhutitypename=&zhuti=&currentPage=1&pagesize=10"
     base_ps = get_html(base_url)  #获取html文本
     go()
